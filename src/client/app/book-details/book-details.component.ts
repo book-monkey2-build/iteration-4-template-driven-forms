ad7ebb48963ffb8fcd4aaa4fb606bcc1e09e3b30
The Buildbot <buildbot@angular2buch.de>
2016-04-19 17:36:18 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/205fc0e8608da5be9341052f4fd4dcc050b52274
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/124251661

---
+++ src/client/app/book-details/book-details.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input} from 'angular2/core';
+import {RouteParams, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Book} from '../../domain/book';
+import {BookStoreService} from '../services/books/book-store.service';
+
+@Component({
+  selector: 'book-details',
+  moduleId: module.id,
+  templateUrl: 'book-details.html',
+  providers: [BookStoreService],
+  directives: [ROUTER_DIRECTIVES]
+})
+export class BookDetailsComponent {
+  book: Book;
+
+  constructor(private params: RouteParams, private bs: BookStoreService) {
+    this.book = bs.getSingle(params.get('isbn'));
+  }
+
+  getRating(num: number){
+    return new Array(num);
+  }
+}
---
+++ src/client/app/book-list/book-list.component.ts
@@ -0,0 +1,20 @@
+import {Component} from 'angular2/core';
+import {RouterLink} from 'angular2/router';
+import {BookListItem} from '../book-list-item/book-list-item.component';
+import {Book} from '../../domain/book';
+import {BookStoreService} from '../services/books/book-store.service';
+
+@Component({
+  selector: 'book-list',
+  directives: [BookListItem, RouterLink],
+  moduleId: module.id,
+  templateUrl: 'book-list.component.html',
+  providers: [BookStoreService]
+})
+export class BookListComponent {
+  books: Book[];
+
+  constructor(private bs: BookStoreService) {
+    this.books = bs.getAll();
+  }
+}
