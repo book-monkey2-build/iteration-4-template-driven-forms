aebf120f16fdcda98770e705f4539278d27999bc
The Buildbot <buildbot@angular2buch.de>
2016-10-08 22:02:54 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/a142efdb784f603b96ec6e6188d0437b50264bd0
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/166115223

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +10,7 @@ import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookFormComponent } from './book-form/book-form.component';
 
 import { BookStoreService } from './shared/book-store.service';
-import { AppRoutingModule } from './app-routing.module.one-app';
+import { AppRoutingModule } from './app-routing.module';
 import { DateValueAccessorModule } from 'angular2-date-value-accessor';
 
 @NgModule({
@@ -23,7 +23,7 @@ import { DateValueAccessorModule } from 'angular2-date-value-accessor';
     BookFormComponent // NEW
   ],
   imports: [
-    CommonModule,
+    BrowserModule,
     FormsModule, // NEW
     AppRoutingModule,
     DateValueAccessorModule
